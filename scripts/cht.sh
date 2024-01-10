e098e69e3d52b9473466fcc38cd757d6d40959aa
Patryk Dyndał <pady@dhigroup.com>
2024-01-10 12:27:32 +0100

    Added scripts

---
+++ scripts/cht.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+declare -a languages=("python" "go" "rust" "javascript")
+declare -a core_utils=("curl" "wget" "tmux")
+
+declare -a all_utils=("${languages[@]}" "${core_utils[@]}")
+
+selected=$(printf '%s\n' "${all_utils[@]}" | fzf)
+
+read -p "Enter the search term: " search_term
+
+parsed_search_term=$(echo $search_term | tr ' ' '+')
+
+if [[ " ${languages[@]} " =~ " ${selected} " ]]; then
+    curl cht.sh/$selected/$search_term | less
+else
+    curl cht.sh/$selected~$parsed_search_term | less
+fi
+
